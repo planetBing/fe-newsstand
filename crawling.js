8a8b5e8a72e070821713a97ba1f5e940c636633e
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-03-05 16:17:28 +0900

    axios와 cheerio를 이용한 크롤링

--- crawling.js
+++ crawling.js
@@ -1,17 +1,26 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const getHTML = async(url) => {
-    try {
-        return await axios.get(url);
-    } catch(err) {
-        console.log(err);
-    }
-}
+const getHTML = async (url) => {
+  try {
+    return await axios.get(url);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const parsing = async(url) => {
-    const html = await getHTML(url);
-    console.log(html);
-}
+const parsing = async (url) => {
+  const html = await getHTML(url);
+  console.log(html);
+  const $ = cheerio.load(html.data);
+  const $news_logo = $(".news_logo");
+  //   console.log($news_logo);
 
-parsing('https://www.naver.com/');
+  const newsLogos = [];
+  $news_logo.each((idx, element) => {
+    const src = $(element).attr("src");
+    console.log(src);
+  });
+};
+
+parsing("https://www.naver.com/");
