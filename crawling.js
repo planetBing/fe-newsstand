79366684fe232804cfb38beadcc677bd3a763bb0
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-03-06 13:22:21 +0900

    feat. 다음 페이지의 언론사 이미지 크롤링

--- crawling.js
+++ crawling.js
@@ -1,5 +1,7 @@
 // const axios = require("axios");
 // const cheerio = require("cheerio");
+const pupppeteer = require("puppeteer");
+const fs = require("fs");
 
 // const getHTML = async (url) => {
 //   try {
@@ -26,21 +28,63 @@
 
 // parsing("https://www.naver.com/");
 
-const pupppeteer = require("puppeteer");
-
 async function main() {
   const browser = await pupppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
   await page.goto("https://www.naver.com/");
-  const ehList = await page.$$(
+
+  const ehList1 = await page.$$(
     ".MediaSubscriptionView-module__news_thumb___IA4y2"
   );
-  for (let eh of ehList) {
+  const imgSrcArr = [];
+  for (let eh of ehList1) {
     const imgSrc = await eh.$eval("img", function (el) {
       return el.src;
     });
-    console.log(imgSrc);
+    const imgObj = { img: imgSrc };
+    imgSrcArr.push(imgObj);
   }
+
+  await page.click(".ContentPagingView-module__btn_next___ZBhby");
+  const ehList2 = await page.$$(
+    ".MediaSubscriptionView-module__news_thumb___IA4y2"
+  );
+  for (let eh of ehList2) {
+    const imgSrc = await eh.$eval("img", function (el) {
+      return el.src;
+    });
+    const imgObj = { img: imgSrc };
+    imgSrcArr.push(imgObj);
+  }
+
+  await page.click(".ContentPagingView-module__btn_next___ZBhby");
+  const ehList3 = await page.$$(
+    ".MediaSubscriptionView-module__news_thumb___IA4y2"
+  );
+  for (let eh of ehList3) {
+    const imgSrc = await eh.$eval("img", function (el) {
+      return el.src;
+    });
+    const imgObj = { img: imgSrc };
+    imgSrcArr.push(imgObj);
+  }
+
+  await page.click(".ContentPagingView-module__btn_next___ZBhby");
+  const ehList4 = await page.$$(
+    ".MediaSubscriptionView-module__news_thumb___IA4y2"
+  );
+  for (let eh of ehList4) {
+    const imgSrc = await eh.$eval("img", function (el) {
+      return el.src;
+    });
+    const imgObj = { src: imgSrc };
+    imgSrcArr.push(imgObj);
+  }
+
+  const jsonImgData = JSON.stringify(imgSrcArr);
+  fs.writeFileSync("logoImg.json", jsonImgData);
+
   browser.close();
 }
 
