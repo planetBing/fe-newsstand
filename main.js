4268769987ae14638d2966a588bd7e410f63db1b
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-03-07 18:28:33 +0900

    refactor: 함수 리팩토링

--- main.js
+++ main.js
@@ -2,22 +2,30 @@ let currentPage = 0;
 const itemsPerPage = 24;
 
 async function main() {
-  //로고 json 데이터 가져오고
-  //첫번째 페이지의 화면 보여주기
-  //버튼 클릭 이벤트리스너 추가 => 버튼 누르면 currentPage 증가, 자식 요소 추가하는 콜백함수
   const logoSrcArr = await getLogoImgSrc();
   const pressGridEl = document.querySelector(".press-grid");
+  const nextButton = document.querySelector(".right-button");
+  const prevButton = document.querySelector(".left-button");
   const pageData = { currentPage, itemsPerPage };
 
   viewPressLogo(pageData, logoSrcArr, pressGridEl);
 
-  const nextButton = document.querySelector(".right-button");
   nextButton.addEventListener("click", (event) => {
     event.preventDefault();
     pageData.currentPage++;
     clearPressGrid();
     viewPressLogo(pageData, logoSrcArr, pressGridEl);
-    if (pageData.currentPage === 3) nextButton.remove();
+    if (pageData.currentPage === 3) nextButton.classList.add("hidden");
+    if (pageData.currentPage !== 0) prevButton.classList.remove("hidden");
+  });
+
+  prevButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    pageData.currentPage--;
+    clearPressGrid();
+    viewPressLogo(pageData, logoSrcArr, pressGridEl);
+    if (pageData.currentPage !== 3) nextButton.classList.remove("hidden");
+    if (pageData.currentPage === 0) prevButton.classList.add("hidden");
   });
 }
 
@@ -25,8 +33,7 @@ async function getLogoImgSrc() {
   try {
     const response = await fetch("logoImg.json");
     const imgData = await response.json();
-    const arr = Object.values(imgData).map((imgObj) => imgObj);
-    const imgSrcArr = arr.map((obj) => obj.src);
+    const imgSrcArr = Object.values(imgData).map((obj) => obj.src);
     return imgSrcArr;
   } catch (err) {
     console.error("JSON 파일을 가져오는 도중 에러 발생.", error);
@@ -37,12 +44,18 @@ async function getLogoImgSrc() {
 function viewPressLogo(pageData, logoSrcArr, pressGridEl) {
   const startIndex = pageData.currentPage * itemsPerPage;
   const endIndex = startIndex + pageData.itemsPerPage;
+  const logoIndex = { startIndex, endIndex };
 
-  logoSrcArr.slice(startIndex, endIndex).forEach((src) => {
+  addChildEl(pressGridEl, logoSrcArr, logoIndex);
+}
+
+function addChildEl(pressGridEl, logoSrcArr, logoIndex) {
+  logoSrcArr.slice(logoIndex.startIndex, logoIndex.endIndex).forEach((src) => {
     const newPressBox = document.createElement("div");
     const newsLogo = document.createElement("img");
     newsLogo.src = src;
     newPressBox.classList.add("press-box");
+    newsLogo.classList.add("press-logo");
     newPressBox.appendChild(newsLogo);
     pressGridEl.appendChild(newPressBox);
   });
