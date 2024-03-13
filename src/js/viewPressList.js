2d3fbe50028dcf1acab8ab85ed99c0e9e9c0efed
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-03-13 17:01:57 +0900

    feat: press list 간단 설계

--- src/js/viewPressList.js
+++ src/js/viewPressList.js
@@ -1,3 +1,6 @@
+let currentPage = 1;
+let totalPage = 81;
+
 export function switchToListByViewer() {
   const listViewer = document.querySelector(".viewer-list");
   const girdViewer = document.querySelector(".viewer-grid");
@@ -10,4 +13,19 @@ export function switchToListByViewer() {
     listWrap.classList.remove("display-none");
     gridWrap.classList.add("display-none");
   });
+}
+
+function initPressListView() {
+  //초기 pressListView 화면 보여주기
+}
+
+function loadAndSetNewsPages() {
+  // 상황1. 페이지 끝까지 도달한 경우 2. category nav를 클릭한 경우
+  //카테고리에 따른 데이터 불러오기
+  //데이터에 따른 페이지 설정
+}
+
+function renderPagesByCategory() {
+  //다음 또는 이전 버튼을 눌렀을 때
+  //20초가 지난 경우
 }
