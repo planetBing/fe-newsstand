84157a2a6982d8e753d08c0b5f1505c7405ee956
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-03-15 14:00:59 +0900

    feat: 새로고침 시 언론사 순서 랜덤으로

--- data/categoryDictionary.js
+++ data/categoryDictionary.js
@@ -9,11 +9,15 @@ import {
 } from "./pressList.js";
 
 export const pressData = [
-  { category: "종합/경제", pressList: economy },
-  { category: "방송/통신", pressList: broadCast },
-  { category: "IT", pressList: it },
-  { category: "영자지", pressList: english },
-  { category: "스포츠/연예", pressList: sports },
-  { category: "매거진/전문지", pressList: magazine },
-  { category: "지역", pressList: local },
+  { category: "종합/경제", pressList: shufflePressList(economy) },
+  { category: "방송/통신", pressList: shufflePressList(broadCast) },
+  { category: "IT", pressList: shufflePressList(it) },
+  { category: "영자지", pressList: shufflePressList(english) },
+  { category: "스포츠/연예", pressList: shufflePressList(sports) },
+  { category: "매거진/전문지", pressList: shufflePressList(magazine) },
+  { category: "지역", pressList: shufflePressList(local) },
 ];
+
+function shufflePressList(pressArr) {
+  return pressArr.sort(() => Math.random() - 0.5);
+}
